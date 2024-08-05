3e80f0bc662e21be7d5a248878059c465e2dc976
Pigmuffin5970 <157852533+Pigmuffin5970@users.noreply.github.com>
2024-08-04 18:33:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('home-screen').style.display = 'none';
+    document.getElementById('game-screen').style.display = 'block';
+    startGame();
+});
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const box = 20;
+const canvasSize = canvas.width;
+
+let snake = [];
+snake[0] = { x: 9 * box, y: 10 * box };
+
+let food = {
+    x: Math.floor(Math.random() * (canvasSize / box)) * box,
+    y: Math.floor(Math.random() * (canvasSize / box)) * box
+};
+
+let direction;
+
+document.addEventListener("keydown", directionControl);
+
+function directionControl(event) {
+    if (event.keyCode == 37 && direction != "RIGHT") {
+        direction = "LEFT";
+    } else if (event.keyCode == 38 && direction != "DOWN") {
+        direction = "UP";
+    } else if (event.keyCode == 39 && direction != "LEFT") {
+        direction = "RIGHT";
+    } else if (event.keyCode == 40 && direction != "UP") {
+        direction = "DOWN";
+    }
+}
+
+function collision(newHead, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (newHead.x == array[i].x && newHead.y == array[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function startGame() {
+    if (typeof game != "undefined") clearInterval(game);
+    game = setInterval(draw, 100);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i == 0 ? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x, food.y, box, box);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "LEFT") snakeX -= box;
+    if (direction == "UP") snakeY -= box;
+    if (direction == "RIGHT") snakeX += box;
+    if (direction == "DOWN") snakeY += box;
+
+    if (snakeX == food.x && snakeY == food.y) {
+        food = {
+            x: Math.floor(Math.random() * (canvasSize / box)) * box,
+            y: Math.floor(Math.random() * (canvasSize / box)) * box
+        };
+    } else {
+        snake.pop();
+    }
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    };
+
+    if (snakeX < 0 || snakeY < 0 || snakeX >= canvasSize || snakeY >= canvasSize || collision(newHead, snake)) {
+        clearInterval(game);
+        alert("Game Over");
+    }
+
+    snake.unshift(newHead);
+}
